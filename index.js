30b786a14b802793331af4abdaca21a37eb59303
sanhyu <qimsh7@gmail.com>
2021-04-21 20:42:26 +0900

    Add files

---
+++ index.js
@@ -0,0 +1,29 @@
+const sansInfo = {
+    name : "KimSangHyuk",
+    age : 24,
+    gender : "Male",
+    favMusicians : [
+        {
+        name : "Crush",
+        gender : "male"
+        },
+        {
+        name : "Yerin Baek",
+        gender : "female"
+        },
+        {
+        name : "Tyler, the creator",
+        gender : "male"
+        },
+        {
+        name : "Bill Evans",
+        gender : "male"
+        },
+        {
+        name : "Jun-il Jung",
+        gender : "male"
+        }
+    ]
+}
+
+console.log(sansInfo.favMusicians[1].name);
