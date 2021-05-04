f182919938853cc2fa24acd930b77e832ff27208
sanhyu <qimsh7@gmail.com>
2021-05-04 21:10:39 +0900

    2nd day

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const sansInfo = {
-    name : "KimSangHyuk",
-    age : 24,
-    gender : "Male",
-    favMusicians : [
-        {
-        name : "Crush",
-        gender : "male"
-        },
-        {
-        name : "Yerin Baek",
-        gender : "female"
-        },
-        {
-        name : "Tyler, the creator",
-        gender : "male"
-        },
-        {
-        name : "Bill Evans",
-        gender : "male"
-        },
-        {
-        name : "Jun-il Jung",
-        gender : "male"
-        }
-    ]
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+    /* ==
+    const hasClass = title.classList.contains(CLICKED_CLASS)
+    if(hasClass){
+        title.classList.remove(CLICKED_CLASS);
+    }
+    else{
+        title.classList.add(CLICKED_CLASS);
+    }
+    */
 }
 
-console.log(sansInfo.favMusicians[1].name);
+function init(){
+    title.addEventListener("click", handleClick);
+}
+init();
